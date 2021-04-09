b2cbfba3675f08995f17c04d23cedf1369160a97
AAKASH <aakashaggarwal2311@gmail.com>
2021-04-10 00:44:56 +0530

    lecture 10 usestate blog hook

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,23 @@
 import {useState} from 'react';
 
 const Home = () => {
+const [blogs,setBlogs] = useState([
+  { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+]);
 
- // let name ='mahima'; 
- const [name,setName]= useState('mahima'); 
- const [age,setage] =useState(25);
- const handleClick=() =>
-  {
-    console.log(name);
-    setName('didi');
-    setage(21);
-  }
- 
     return ( 
         <div className="home">
-          <h2>Homepage</h2> 
-          <p>{name} is age:{age} </p>
-          <button onClick={handleClick}>Click me</button> 
+          {
+            blogs.map( (blog) => 
+             (
+              <div className="blog-preview" key={blog.id}>
+                <h2>{blog.title}</h2>
+                <p>writen by {blog.author}</p>
+              </div>
+            ))
+          }
         </div>
      );
 }
